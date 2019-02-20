7a5f14c38b4d618a0a30f9050eb1e937f303a861
a_maulana <cintamupalsu@hotmail.com>
2019-02-20 15:13:00 +0900

    Add devise 01

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'main_pages#home'
   get 'main_pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
